2238b46ae9ebaa8b6c5022fb8049edc144b59ab3
Aiste Zuperkaite <aistonija16@gmail.com>
2020-12-29 13:44:00 +0200

    Recap - FOR cycles - Electricity task

--- index.php
+++ index.php
@@ -1,80 +1,20 @@
 <?php
 
-$users = [
-    [
-        'user_name' => 'Black Widow',
-        'user_email' => 'b_widow@gmail.com',
-        'user_password' => 'widdow2',
-        'user_img' => 'https://cdn.vox-cdn.com/thumbor/9npVmC2tPdivC0gXXYvGWSsGwO8=/0x0:1300x650/1200x800/filters:focal(421x164:629x372)/cdn.vox-cdn.com/uploads/chorus_image/image/65815896/black_widow_marvel.0.jpeg',
-    ],
-    [
-        'user_name' => 'Captain America',
-        'user_email' => 'captain_america@gmail.com',
-        'user_password' => '1maCap',
-        'user_img' => 'https://www.geo.tv/assets/uploads/updates/2020-04-07/281462_031504_updates.jpg',
-    ],
-    [
-        'user_name' => 'Gomora',
-        'user_email' => 'galaxy_guardian@gmail.com',
-        'user_password' => 'Galaxxxx',
-        'user_img' => 'https://www.denofgeek.com/wp-content/uploads/2019/11/Gamora_Guardians_of_the_Galaxy.jpg?fit=1200%2C675',
-    ],
-    [
-        'user_name' => 'Star Lord',
-        'user_email' => 'real_guardian@gmail.com',
-        'user_password' => 'starL000rd',
-        'user_img' => 'https://i.ytimg.com/vi/hu-bqCChJyc/maxresdefault.jpg',
-    ],
-];
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Users</title>
-    <style>
-        .container {
-            width: 900px;
-            margin: 0 auto;
-            display: flex;
-        }
+$month_days = 31;
+$current_day = rand(1, 31);
+$kwh_price = 0.07;
 
-        .card {
-            width: calc(300px - 20px);
-            margin: 10px;
-            box-shadow: 0 2px 4px #bcbcbc;
-        }
+$total_kwh = 0;
 
-        .img {
-            height: 200px;
-        }
+for ($day = 1; $day <= $month_days; $day++) {
+    if ($day <= $current_day) {
+        $kwh_used = rand(2, 5);
+        $total_kwh += $kwh_used;
+    } else {
+        break;
+    }
+}
 
-        .img img {
-            width: 100%;
-            height: 200px;
-            object-fit: cover;
-        }
+$total_bill = number_format($kwh_price * $total_kwh, 2);
+var_dump($total_bill);
 
-        .card-body {
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-<div class="container">
-    <?php foreach ($users as $user): ?>
-        <div class="card">
-            <div class="img">
-                <img src="<?php print $user['user_img']; ?>">
-            </div>
-            <div class="card-body">
-                <h3>User name: <?php print $user['user_name']; ?></h3>
-                <p>Password: <?php print $user['user_password']; ?></p>
-                <p>email: <?php print $user['user_email']; ?></p>
-            </div>
-        </div>
-    <?php endforeach; ?>
-</div>
-</body>
-</html>
