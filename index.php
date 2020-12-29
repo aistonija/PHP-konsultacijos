712fd3b83b384c8ba7a7345707c3648a9f97345b
Aiste Zuperkaite <aistonija16@gmail.com>
2020-12-29 14:14:15 +0200

    Recap - FOR cycles - RGB

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
-<?php
-
-$month_days = 31;
-$current_day = rand(1, 31);
-$kwh_price = 0.07;
-
-$total_kwh = 0;
-
-for ($day = 1; $day <= $month_days; $day++) {
-    if ($day <= $current_day) {
-        $kwh_used = rand(2, 5);
-        $total_kwh += $kwh_used;
-    } else {
-        break;
-    }
-}
-
-$total_bill = number_format($kwh_price * $total_kwh, 2);
-var_dump($total_bill);
-
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        div {
+            width: 300px;
+            height: 25px;
+            margin: 10px auto;
+        }
+    </style>
+</head>
+<body>
+<?php for ($x = 1; $x <= 15; $x++): ?>
+    <div style="background: rgb( 0, 0, <?php print 255 * ($x / 15); ?>)"></div>
+<?php endfor; ?>
+</body>
+</html>
