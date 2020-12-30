a4a508c32f1071292fea38747edcab31c9aa8787
Aiste Zuperkaite <aistonija16@gmail.com>
2020-12-30 14:57:01 +0200

    Recap - Arrays - Reversed Array Values task

--- index.php
+++ index.php
@@ -1,26 +1,67 @@
 <?php
 
 $array = [
-    'first' => 7,
-    'second' => 2,
-    'third' => 8,
-    'fourth' => 267,
-    'fifth' => 5,
-    'sixth' => 7
+    [1, 1, 1],
+    [0, 1, 0],
+    [1, 0, 0],
 ];
 
-var_dump($array);
+$reversed_array = [];
 
-$every_second = [];
-
-$counter = 0;
-
-foreach ($array as $key => $value) {
-    $counter++;
-
-    if ($counter % 2 === 0) {
-        $every_second[$key] = $value;
+foreach ($array as $array_index => $row) {
+    foreach ($row as $row_index => $value) {
+        if ($value === 0) {
+            $reversed_array[$array_index][$row_index] = 1;
+        } else {
+            $reversed_array[$array_index][$row_index] = 0;
+        }
     }
 }
 
-var_dump($every_second);
+var_dump($array);
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        table {
+            margin: 50px auto;
+            border: 1px solid #777777;
+            border-collapse: collapse;
+        }
+
+        table tr td {
+            width: 50px;
+            height: 50px;
+            text-align: center;
+            border: 1px solid #777777;
+        }
+    </style>
+    <title>Reverse Array Values</title>
+</head>
+<body>
+<table>
+    <?php foreach ($array as $row) : ?>
+        <tr>
+            <?php foreach ($row as $value) : ?>
+                <td><?php print $value; ?></td>
+            <?php endforeach; ?>
+        </tr>
+    <?php endforeach; ?>
+</table>
+<table>
+    <?php foreach ($reversed_array as $row) : ?>
+        <tr>
+            <?php foreach ($row as $value) : ?>
+                <td><?php print $value; ?></td>
+            <?php endforeach; ?>
+        </tr>
+    <?php endforeach; ?>
+</table>
+</body>
+</html>
