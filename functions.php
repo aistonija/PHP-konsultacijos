ff36327b05838c1d649c328fac4e7a3ea9c7a178
Aiste Zuperkaite <aistonija16@gmail.com>
2021-01-07 21:24:20 +0200

    Forms - Register - Add New User to Array via Reference

--- functions.php
+++ functions.php
@@ -12,3 +12,23 @@ function pass_match($pass1, $pass2)
     return $pass1 === $pass2;
 }
 
+/**
+ * Check if user exists in array, and add new one in case of false
+ *
+ * @param $users_array
+ * @param $temporary_array
+ * @param $email
+ * @return bool
+ */
+function user_exists(&$users_array, $temporary_array, $email)
+{
+    foreach ($users_array as $user) {
+        if ($user['email'] === $email) {
+            return false;
+        }
+    }
+
+    $users_array[] = $temporary_array;
+
+    return true;
+}
--- process.php
+++ process.php
@@ -6,17 +6,36 @@ require_once 'users.php';
 $message = '';
 $message_class = '';
 
+$temporary_users = [];
+
+if (!empty($_POST)) {
+    $temporary_users['email'] = $_POST['email'];
+    $temporary_users['password'] = $_POST['password'];
+}
+
 if (isset($_POST['login'])) {
     $pass1 = $_POST['password'];
     $pass2 = $_POST['password2'];
 
     $pass_valid = pass_match($pass1, $pass2);
+
     if ($pass_valid) {
-        $message = 'Slaptažodžiai sutapo, sveikiname užsiregistravus';
-        $message_class = 'message';
+        $user_registered = user_exists($users, $temporary_users, $_POST['email']);
+
+        if ($user_registered) {
+            $message = 'Sveikiname užsiregistravus!';
+            $message_class = 'message';
+
+        } else {
+            $message = 'Vartotojas ' . $_POST['email'] . ' jau egzistuoja!';
+            $message_class = 'error';
+        }
+
     } else {
         $message = 'Slaptažodžiai NESUTAMPA';
         $message_class = 'error';
     }
 
 }
+
+var_dump($users);
